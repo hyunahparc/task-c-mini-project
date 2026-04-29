a54cd4cf762c7306b13a920c6dc44552beb9453f
hyunahparc <hyunah.parc@gmail.com>
2026-04-29 02:48:13 +0200

    feat: configure DbContext and relationships

---
+++ TaskFlow/Data/ApiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.Models;
+
+namespace TaskFlow.Data
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectTask> ProjectTasks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
